42bd8e0b5c1526afd1dcf13eb0cfe7ef21e5ffa3
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-23 14:45:56 +0530

    DASHBOARD

--- script.js
+++ script.js
@@ -4,12 +4,31 @@ setInterval(() => {
     t.innerHTML = time.toLocaleTimeString();
 }, 1000);
 
-function login()
-{
+function login() {
 
 }
 
 function mode() {
     var element = document.body;
     element.classList.toggle("mode");
- }
+}
+
+function prt() {
+    window.print();
+}
+function submit()
+{
+    window.open("http://127.0.0.1:5500/HtmlFiles/table.html");  
+}
+
+function next()
+{
+    alert("No more...")
+}
+
+setInterval(()=>
+{
+    let d = new Date();
+    let date = d.toDateString();
+    document.getElementById('date').innerHTML = date;
+})
