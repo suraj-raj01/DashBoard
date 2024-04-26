1d53acb1bda8d47fdc8a83eeb6f711ea0329b875
SURAJ_KUMAR <surajk38656@gmail.com>
2024-04-26 14:41:36 +0530

    DashBoard

--- script.js
+++ script.js
@@ -82,3 +82,43 @@ function learMore()
 {
     window.open("https://www.canarahsbclife.com/blog/financial-planning/what-are-financial-goals-meaning-types-and-benefits","width=600px","height=600px","left=450px","top=100px")
 }
+
+
+// TEAM 
+
+async function fetchData()
+{
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let j = await a.json();
+    let ot = "";
+    let s = document.getElementById('displayData');
+    let res = j.map((e)=>ot+=`
+
+    <tr>
+     <td>${e.id}</td>
+     <td>${e.userId}</td>
+     <td>${e.title}</td>
+    </tr>
+    `
+).join(" ");
+s.innerHTML = res;
+}
+
+
+async function fetchData1()
+{
+    let a = await fetch("https://jsonplaceholder.typicode.com/albums/1/photos");
+    let j = await a.json();
+    let ot = "";
+    let s = document.getElementById('displayData1');
+    let res = j.map((e)=>ot+=`
+
+    <tr>
+     <td>${e.albumId}</td>
+     <td>${e.id}</td>
+     <td>${e.url}</td>
+    </tr>
+    `
+).join(" ");
+s.innerHTML = res;
+}
