5e727e0f6ef952e291b270baf01483848ec80d6a
SURAJ_KUMAR <surajk38656@gmail.com>
2024-06-07 12:38:43 +0530

    DashBoard

--- script.js
+++ script.js
@@ -64,6 +64,7 @@ async function fet() {
  <td>${e.course}</td>
  <td>${e.grade}</td>
  <td><button onclick="mydelete('${e.id}')">Delete</button></td>
+ <td><button onclick="mymodify('${e.id}')">Modify</button></td>
  </tr>
 `).join(" ");
     p.innerHTML = s;
@@ -75,85 +76,12 @@ function mydelete(id)
     fetch(`http://localhost:3000/student/${id}`,{
         method:"DELETE"
     })
-    .then(res=>alert("data deleted Successfulley"))
+    .then(res=>alert("Do you want to Delete Data"))
 }
 
-let perc = document.getElementById('perc');
-function per() {
-    setInterval(() => {
-        let n = Math.ceil(Math.random() * 10);
-        perc.innerHTML = n;
-    }, 400)
+function mymodify(id)
+{
+    fetch(`http://localhost:3000/student/${id}`,{
+        method:""
+    })
 }
-per();
-
-let ghz = document.getElementById('ghz');
-function gh() {
-    setInterval(() => {
-        let n = (Math.random() * 10.5);
-        ghz.innerHTML = n;
-    }, 400)
-}
-gh();
-
-// lear More Function
-function learMore() {
-    window.open("https://www.canarahsbclife.com/blog/financial-planning/what-are-financial-goals-meaning-types-and-benefits", "width=600px", "height=600px", "left=450px", "top=100px")
-}
-
-
-// TEAM 
-
-async function fetchData() {
-    let a = await fetch("https://jsonplaceholder.typicode.com/posts");
-    let j = await a.json();
-    let ot = "";
-    let s = document.getElementById('displayData');
-    let res = j.map((e) => ot += `
-
-    <tr>
-     <td>${e.id}</td>
-     <td>${e.userId}</td>
-     <td>${e.title}</td>
-    </tr>
-    `
-    ).join(" ");
-    s.innerHTML = res;
-}
-
-
-async function fetchData1() {
-    let a = await fetch("https://jsonplaceholder.typicode.com/albums/1/photos");
-    let j = await a.json();
-    let ot = "";
-    let s = document.getElementById('displayData1');
-    let res = j.map((e) => ot += `
-
-    <tr>
-     <td>${e.albumId}</td>
-     <td>${e.id}</td>
-     <td><img src="${e.url}"></td>
-    </tr>
-    `
-    ).join(" ");
-    s.innerHTML = res;
-}
-
-
-
-// Insert Items
-
-var firstname = document.getElementById("firstname")
-var lastname = document.getElementById("lastname")
-var jsonBtn = document.getElementById("jsonbtn")
-var jsonText = document.getElementById("jsontext")
-
-
-
-jsonBtn.addEventListener("click", function () {
-    var data = {
-        "firstName": firstname.value,
-        "lastName": lastname.value
-    }
-    jsonText.innerHTML = JSON.stringify(data)
-})
